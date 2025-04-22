9b10e5b5f347605367486dd1f790f47e003e4189
drtjsgh <troy@drtjstephenson.com>
2025-04-23 09:26:14 +1000

    smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,4 @@ if ingredients_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
